aa9153f2a20a7b120f260f2008ecea39e1cf202c
devdesignerprakash <devdesignerprakash@gmail.com>
2024-11-26 11:45:42 +0545

    äuth service and controllers on progress

---
+++ server/src/controllers/authControllers.js
@@ -0,0 +1,14 @@
+const { authenticateUser } = require("../services/authService");
+
+const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const { token, user } = await authenticateUser(email, password);
+    res.status(200).json({ msg:"logged in Successfully",token, user });
+  } catch (error) {
+    res.status(error.message === "Invalid credentials" || error.message === "User not found" ? 401 : 500).json({
+      message: error.message,
+    });
+  }
+};
+module.exports = { loginController };
---
+++ server/src/services/authService.js
@@ -0,0 +1,18 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const Profile = require("../schemas/profileSchema");
+const authenticateUser = async (email, password) => {
+  const user = await Profile.findOne({ email });
+  if (!user) throw new Error("User not found");
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) throw new Error("Invalid credentials");
+
+  const token = jwt.sign({ id: user._id, userType: user.userType }, process.env.JWT_SECRET,{
+    expiresIn: "1d",
+  });
+
+  return { token, user };
+};
+
+module.exports = { authenticateUser };
